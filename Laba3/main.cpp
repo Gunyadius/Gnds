32c52f06be391ab1c5a20fa08aed859ea0789cb3
Сергей Глушенков <Gunya@macbook-pro-sergej.local>
2017-11-22 01:02:17 +0200

    Initial Commit

---
+++ Laba3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Laba3
+//
+//  Created by Сергей Глушенков on 22.11.17.
+//  Copyright © 2017 Сергей Глушенков. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
